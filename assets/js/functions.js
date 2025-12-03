6552e60525371d1e064b3e147a5d2dcc255a517f
Giovana Merguizo <giovana.pontes5654@hotmail.com>
2025-12-03 14:05:09 -0300

    add experiencia

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,5 @@
 function baixarPdf() {
-  const pdfUrl = "/assets/docs/Curriculo_Giovana_Merguizo.pdf"; // Substitua pelo URL do PDF
+  const pdfUrl = "/assets/docs/curriculo_giovana_merguizo.pdf"; // Substitua pelo URL do PDF
   
   fetch(pdfUrl)
     .then(response => response.blob())
@@ -7,7 +7,7 @@ function baixarPdf() {
       const url = window.URL.createObjectURL(blob);
       const a = document.createElement('a');
       a.href = url;
-      a.download = 'curriculo_giovana_merguizo.pdf'; // Nome desejado para o arquivo
+      a.download = 'Curriculo_Giovana_Merguizo.pdf'; // Nome desejado para o arquivo
       document.body.appendChild(a);
       a.click();
       window.URL.revokeObjectURL(url);
